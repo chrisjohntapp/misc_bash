9eebd4911897111a02333f12b8db80ee8f5a9282
Christopher J Tapp <chrisjohntapp@gmail.com>
2017-10-04 16:29:11 +0100

    lib_ssh added

--- scripts/past/deploy-website.sh
+++ scripts/past/deploy-website.sh
@@ -9,12 +9,8 @@ WEBSITE="testproj"
 confirm () {
     read -r -p "${1:-Are you sure? [y/N]} " response
     case $response in
-        [yY][eE][sS]|[yY])
-            true
-            ;;
-        *)
-            false
-            ;;
+        [yY][eE][sS]|[yY]) true ;;
+                        *) false ;;
     esac
 }
 
