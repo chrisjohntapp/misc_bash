e1d0bf81bb62d1880939e03887fc55675d9c3153
Christopher J Tapp <chrisjohntapp@gmail.com>
2017-10-07 22:48:42 +0100

    Comments added to all functions

--- libs/lib_files.sh
+++ libs/lib_files.sh
@@ -1,57 +1,91 @@
 #!/bin/bash
-
+#
+# Utility functions related to file operations.
+#
+# vi:syntax=sh
 # shellcheck disable=SC2034
-_lib_files=1
+_LIB_FILES=1
 
-remove_file_endstrings()
-{
+function remove_file_end_strings() {
+  ##############################################################################
   # Removes a string from the end of all filenames in a given directory.
+  # Globals:
+  #   None
+  # Arguments:
+  #   (optional) directory (The directory in which to operate. Defaults to $CWD)
+  #   (required) string (The string to be removed from all files)
+  # Returns:
+  #   None
+  ##############################################################################
 
   while getopts 'd:' argv; do
-    case $argv in
-      d) target_dir=$OPTARG ;;
-      h) printf "Usage: remove_file_endstrings [ -d <work directory> ] <string to remove>\n" ;;
+    case "${argv}" in
+      d) local target_dir="${OPTARG}" ;;
+      h) printf "Usage: remove_file_end_strings [ -d <work directory> ] <string \
+to remove>\n" ;;
     esac
   done
 
   : ${target_dir:=$PWD}
 
-  for _ in $(seq 2 $OPTIND); do
+  for _ in $(seq 2 "${OPTIND}"); do
     shift
   done
 
   if [[ -z "$1" ]]; then
-    printf "No string supplied."
+    printf "No string supplied.\n"
     return 1
   else
-    endstring=$1
+    local end_string=$1
   fi
 
-  find $target_dir -type f -name "*${endstring}" -print0 | while read -d $'\0' file; do mv "$file" "${file%${endstring}}"; done
-
-  unset target_dir endstring file
+  local file
+  find "${target_dir}" -type f -name "*${end_string}" -print0 \
+    | while read -d $'\0' file; do mv "${file}" "${file%${end_string}}"; done
 }
 
-taketip()
-{
-  local tips_path='Dropbox/CLI_tips'
+function tip() {
+  ##############################################################################
+  # Allows quick writing of cli notes/tips to a specific file within the tips
+  # library. Alternatively prints the whole named file to stdout for reading.
+  # TODO: make cat function (no args) use a pager
+  # Globals:
+  #   None
+  # Arguments:
+  #   (required) type (technology name, also the name of the file to be
+  #     written/read).
+  #   (optional) Any additional text, which will be written to file.
+  # Returns:
+  #   If invoked with more than one argument, returns nothing.
+  #   If invoked with only one argument, returns the content of the file with
+  #     the same name as the first argument.
+  ##############################################################################
+  local -r TIPS_PATH='Dropbox/CLI_tips'
   if [[ $# -gt 1 ]]; then
     f=$1; shift
-    printf "$*" >> ${HOME}/${tips_path}/${f}.txt
+    printf "$*" >> "${HOME}/${TIPS_PATH}/${f}.txt"
   elif [[ $# -eq 1 ]]; then
-    cat ${HOME}/${tips_path}/${1}.txt
+    cat "${HOME}/${TIPS_PATH}/${1}.txt"
   else
-    printf "Usage: taketip [ type ] [ Notes to add to file ]\n"
+    printf "Usage: tip type [ Notes to add to file ]\n"
   fi
 }
 
-edittip()
-{
-  local tips_path='Dropbox/CLI_tips'
+function edit_tip() {
+  ##############################################################################
+  # Opens the named file within the tips library for editing.
+  # Globals:
+  #   None
+  # Arguments:
+  #   filename (the name of the tip file to be edited)
+  # Returns:
+  #   A vim session editing the named file.
+  ##############################################################################
+  local -r TIPS_PATH='Dropbox/CLI_tips'
   if [[ $# -eq 1 ]]; then
-    local filename=${HOME}/${tips_path}/${1}.txt
-    if [[ -w $filename ]]; then
-      vim $filename
+    local filename="${HOME}/${TIPS_PATH}/${1}.txt"
+    if [[ -w "${filename}" ]]; then
+      vim "${filename}"
     else
       printf "File does not exist.\n"
     fi
@@ -59,5 +93,3 @@ edittip()
     printf "Usage: edittip filename\n"
   fi
 }
-
-# vi:syntax=sh
