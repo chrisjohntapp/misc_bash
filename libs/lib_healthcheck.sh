3a24f82ed1c6ee393afed4dd56b908a47ce8b4a2
Christopher J Tapp <chrisjohntapp@gmail.com>
2017-10-07 22:26:32 +0100

    Comments added

--- libs/lib_healthcheck.sh
+++ libs/lib_healthcheck.sh
@@ -1,67 +1,80 @@
 #!/bin/bash
-
+#
+# Utility functions to check health of services.
+#
+# vi:syntax=sh
 # shellcheck disable=SC2034
-_lib_healthcheck=1
+_LIB_HEALTHCHECK=1
 
-check_pingable()
-{
-  # Check if a FQDN can be pinged.
-  # You can allow for occasional dropped packets by adjusting the number of pings
-  # to use in the test (using the -p flag). Only if all pings fail will the fqdn
-  # be classed as down.
+function pingable() {
+  ##############################################################################
+  # Check if a FQDN is pingable, allowing for a flaky network.
+  # You can allow for occasional dropped packets by adjusting the number of
+  # pings to use in the test (using the -p flag). Only if all pings fail will
+  # the FQDN be classed as down.
+  # Globals:
+  #   None
+  # Arguments:
+  #   (optional) -p <number of pings>
+  #   (optional) -a <ip address> (of alternative nameserver)
+  #   (required) FQDN (the target of the test)
+  # Returns:
+  #   Code 0 if FQDN responds to at least one ping.
+  #   Code 1 if logical error.
+  #   Code 2 if FQDN can't be resolved.
+  #   Code 3 if FQDN does not respond to any pings.
+  ##############################################################################
 
   OPTIND=1
   local num_pings=3
-  local alt_dns
 
   while getopts 'p:a:' opt; do
-    case "$opt" in
-      p) num_pings="$OPTARG" ;;
-      a) alt_dns="$OPTARG" ;;
-      *) printf "\nUsage: check_alive [ -p <number of pings> ] [ -a ip address] fqdn\n
-(-p The number of pings to use (default is 3)).\n(-a An alternate DNS server for dig test)\n\n"
-         return 1 ;;
+    case "${opt}" in
+      p) num_pings="${OPTARG}" ;;
+      a) local -r ALT_DNS="${OPTARG}" ;;
+    h|*)
+        printf "\nUsage: check_alive [ -p <number of pings> ] \
+[ -a ip address] FQDN\n
+(-p The number of pings to use (default is 3)).\n(-a An alternate DNS server \
+for dig test)\n\n"
+        return 1
+        ;;
     esac
   done
 
-  # Remove any processed options from argv.
-  for _ in $(seq 2 $OPTIND); do
+  # Remove any options from argv.
+  for _ in $(seq 2 "${OPTIND}"); do
     shift
   done
-
-  # Assign remaining argument to variable.
-  local fqdn=$1
-  if [[ -z "$fqdn" ]]; then
-    printf "No fqdn supplied.\n"
+  # .. and assign remaining argument to FQDN.
+  local -r FQDN=$1
+  if [[ -z "${FQDN}" ]]; then
+    printf "No FQDN supplied.\n"
     return 1
   fi
 
-  # Check if fqdn is resolvable.
+  # Use custom resolver if specified.
   local insert
-  if [[ -n "$alt_dns" ]]; then
-    insert="@${alt_dns} "
+  if [[ -n "${ALT_DNS}" ]]; then
+    insert="@${ALT_DNS} "
   else
     insert=""
   fi
-  if [[ -z "$(dig ${insert}${fqdn} a +short)" ]]; then
-    printf "Cannot resolve that fqdn.\n"
-    return 1
+  # ..and check if resolvable.
+  if [[ -z "$(dig "${insert}""${FQDN}" a +short)" ]]; then
+    printf "Cannot resolve that FQDN.\n"
+    return 2
   fi
 
-  # Ping the target fqdn.
-  for i in $(seq $num_pings); do
-    results[$i]=$(ping -c 1 $fqdn >/dev/null; printf "$?\n")
+  # Ping the target FQDN.
+  for i in $(seq ${num_pings}); do
+    results[$i]="$(ping -c 1 "${FQDN}" >'/dev/null'; printf "$?\n")"
     sleep 1
   done
 
   # If there are no successful pings, check failed.
-  if [[ ! $(printf "%s" "${results[@]}" | grep 0) ]]; then
-    printf "$fqdn is not pingable.\n"
-    return 1
+  if ! [[ "$(printf "%s" "${results[@]}" | grep 0)" ]]; then
+    printf "${FQDN} is not pingable.\n"
+    return 3
   fi
-
-  # Clear vars
-  unset results
 }
-
-# vi:syntax=sh
