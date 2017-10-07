c7c5d7aad9b13ddfdba8913ab2769b210cc12d02
Christopher J Tapp <chrisjohntapp@gmail.com>
2017-10-07 22:03:41 +0100

    find in for loop converted to while loop

--- libs/lib_git.sh
+++ libs/lib_git.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-
-# shellcheck disable=SC2034
-_lib_git=1
-
-pull-all()
-{
-  for i in *; do
-    ls -ld $i
-    cd $i || { printf "cd'ing to $i failed.\n"; return 1; }
-    git pull || { printf "git pull failed.\n"; return 1; }
-    cd ..
-  done
-}
-
+#
+# Git utility functions.
+#
 # vi:syntax=sh
+# shellcheck disable=SC2034
+_LIB_GIT=1
+
+function pull_all() {
+  local repo
+  while IFS= read -r -d '' repo; do
+
+    printf '%s\n' "${repo}"
+    cd "${repo}" || { printf "cd'ing to ${repo} failed.\n"; return 1; }
+
+    if [[ -d "./.git" ]]; then
+      git pull || { printf "git pull failed.\n"; return 1; }
+      cd ..
+    else
+      printf "Does not appear to be a git repo. Continuing to next directory.\n"
+      cd ..
+    fi
+
+  done < <(find "${PWD}" -mindepth -maxdepth 1 -type d -print0)
+}
