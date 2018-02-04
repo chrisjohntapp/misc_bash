499bf339aa0f6b070c9ba23850e72efb08182373
Christopher J Tapp <chrisjohntapp@gmail.com>
2018-02-04 10:55:28 +0000

    while/read simplified

--- scripts/current/slave_gre_tunnel_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export LOCAL_IP=172.16.62.102
-export REMOTE_IP=172.16.62.101
-sudo brctl addbr multibr0
-sudo ip link set multibr0 up
-sudo ip link add lxc-gre type gretap remote $REMOTE_IP local $LOCAL_IP ttl 225
-sudo brctl addif multibr0 lxc-gre
-sudo ip link set lxc-gre up
