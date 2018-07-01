9e6368571ab0d19b60101f674495d01cbdec6c34
Christopher J Tapp <chrisjohntapp@gmail.com>
2018-07-01 10:08:34 +0100

    skip-resolver option added

--- libs/lib_healthcheck.sh
+++ libs/lib_healthcheck.sh
@@ -9,7 +9,8 @@ _LIB_HEALTHCHECK=1
 function pingable() {
     ###########################################################################
     # Check if a FQDN is pingable, allowing for a flaky network. It will allow
-    # for occasional dropped packets by adjusting the number of pings to use in    # the test (using the -p flag). Only if all pings fail will the FQDN be
+    # for occasional dropped packets by adjusting the number of pings to use in
+    # the test (using the -p flag). Only if all pings fail will the FQDN be
     # classed as down.
     ###########################################################################
     local func=$(basename "${FUNCNAME[0]}")
@@ -17,15 +18,17 @@ function pingable() {
     OPTIND=1
     local num_pings
 
-    while getopts 'p:a:' opt; do
+    while getopts ':p:a:s' opt; do
 	case "${opt}" in
 	    p) num_pings="${OPTARG}" ;;
 	    a) local -r alt_resolver="${OPTARG}" ;;
+            s) local -r skip_resolver=yes ;;
 	    *)
 		printf "\nUsage: %s [ -p <number of pings> ] " "${func}"
-                printf "[-a ip address] FQDN\n\n"
+                printf "[-a ip address] [-s] FQDN\n\n"
                 printf "(-p The number of pings to use (default is 3))\n"
-                printf "(-a An alternate DNS server for dig test)\n\n"
+                printf "(-a An alternate DNS server for dig test)\n"
+                printf "(-s Skip name resolution check)\n\n"
 		return 1
 		;;
 	esac
@@ -50,11 +53,13 @@ function pingable() {
 	resolver=""
     fi
 
-    local dig_result="$(dig ${resolver}"${fqdn}" a +short)"
+    if ! [[ "${skip_resolver}" == yes ]]; then
+        local dig_result="$(dig ${resolver}"${fqdn}" a +short)"
 
-    if [[ -z "${dig_result}" ]]; then
-	printf "Cannot resolve that FQDN.\n"
-	return 2
+        if [[ -z "${dig_result}" ]]; then
+            printf "Cannot resolve that FQDN.\n"
+            return 2
+        fi
     fi
 
     for i in $(seq ${num_pings}); do
